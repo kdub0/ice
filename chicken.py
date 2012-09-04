676429c6a98135550ec50c6a426b5674005267e3
kdub <kevin.waugh@gmail.com>
2012-09-03 21:30:29 -0400

    renamed chicken() to create()

--- chicken.py
+++ chicken.py
@@ -1,7 +1,7 @@
 import indexing
 from numpy import *
 
-def chicken():
+def create():
     return (indexing.create((2,2)),
             [array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0], [0.0, 0.0, 1.0]]),
              array([[1.0, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])])
